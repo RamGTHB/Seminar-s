bddbb717c5e90f3eeb1dc6ca79a1eddf73985a64
RamGT <n1g9ab@gmail.com>
2022-01-20 22:23:01 +0300

    Sem006+

--- Sem006/Program.cs
+++ Sem006/Program.cs
@@ -49,74 +49,58 @@
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Найти произведение двух матриц
 
-int[,] CreateMatrix(int row, int col)
-{
-    int[,] Matrix = new int[row, col];
-    for (int i = 0; i < row; i++)
-    {
-        for (int j = 0; j < col; j++)
-        {
-            Matrix[i, j] = new Random().Next(0, row + col);
-        }
-    }
-    return Matrix;
-}
+// int[,] CreateMatrix(int row, int col)
+// {
+//     int[,] Matrix = new int[row, col];
+//     for (int i = 0; i < row; i++)
+//     {
+//         for (int j = 0; j < col; j++)
+//         {
+//             Matrix[i, j] = new Random().Next(0, row + col);
+//         }
+//     }
+//     return Matrix;
+// }
 
-void ShowMatrix(int[,] matrixToShow, string nameOfMatrix)
-{
-    Console.WriteLine($"{nameOfMatrix}");
-    for (int i = 0; i < matrixToShow.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrixToShow.GetLength(1); j++)
-        {
-            Console.Write($"{matrixToShow[i, j]}\t");
-        }
-        Console.WriteLine();
-    }
-}
+// void ShowMatrix(int[,] matrixToShow, string nameOfMatrix)
+// {
+//     Console.WriteLine($"{nameOfMatrix}");
+//     for (int i = 0; i < matrixToShow.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrixToShow.GetLength(1); j++)
+//         {
+//             Console.Write($"{matrixToShow[i, j]}\t");
+//         }
+//         Console.WriteLine();
+//     }
+// }
 
-int[,] ProductMatrix(int[,] matrix1, int[,] matrix2)
-{
-    int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
-    for (var i = 0; i < matrix1.GetLength(0); i++)
-    {
-        for (var j = 0; j < matrix2.GetLength(1); j++)
-        {
-            for (var k = 0; k < matrix1.GetLength(1); k++)
-            {
-                resultMatrix[i, j] += matrix1[i,k] * matrix2[k,j];
-            }
-        }
-    }
-    return resultMatrix;
-}
+// int[,] ProductMatrix(int[,] matrix1, int[,] matrix2)
+// {
+//     int[,] resultMatrix = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+//     for (var i = 0; i < matrix1.GetLength(0); i++)
+//     {
+//         for (var j = 0; j < matrix2.GetLength(1); j++)
+//         {
+//             for (var k = 0; k < matrix1.GetLength(1); k++)
+//             {
+//                 resultMatrix[i, j] += matrix1[i,k] * matrix2[k,j];
+//             }
+//         }
+//     }
+//     return resultMatrix;
+// }
 
-int[,] matrix1 = CreateMatrix(2,3);
-ShowMatrix(matrix1, "Первая матрица");
-int[,] matrix2 = CreateMatrix(3, 2);
-ShowMatrix(matrix2, "Вторая матрица");
-int[,] resultMatrix = ProductMatrix(matrix1, matrix2);
-ShowMatrix(resultMatrix, "Произведение матриц");
+// int[,] matrix1 = CreateMatrix(2,3);
+// ShowMatrix(matrix1, "Первая матрица");
+// int[,] matrix2 = CreateMatrix(3,2);
+// ShowMatrix(matrix2, "Вторая матрица");
+// int[,] resultMatrix = ProductMatrix(matrix1, matrix2);
+// ShowMatrix(resultMatrix, "Произведение матриц");
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // В двумерном массиве целых чисел. Удалить строку и столбец, на пересечении которых расположен наименьший элемент.
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Сформировать трехмерный массив не повторяющимися двузначными числами показать его построчно на экран выводя индексы соответствующего элемента
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Показать треугольник Паскаля *Сделать вывод в виде равнобедренного треугольника
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// Спирально заполнить двумерный массив:
-//   1  2  3  4
-//  12 13 14  5
-//  11 16 15  6
-//  10  9  8  7 
 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
