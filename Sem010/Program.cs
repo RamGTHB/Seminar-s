96f003a928427dd62b3026c779bb1cbf324514d8
RamGT <n1g9ab@gmail.com>
2022-01-31 22:27:12 +0300

    add sem010+

--- Sem010/Program.cs
+++ Sem010/Program.cs
@@ -2,20 +2,48 @@
 
 // Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
 
-void SummRec(int firstNumber, int secondNumber, int count, int element)
-{
-    if (count < element)
-    {
-        int sum = firstNumber + secondNumber;
-        Console.Write("{0} ", sum);
-        count++;
-        SummRec(secondNumber, sum, count, element);
-    }
-}
+// void SummRec(int firstNumber, int secondNumber, int count, int element)
+// {
+//     if (count < element)
+//     {
+//         int sum = firstNumber + secondNumber;
+//         Console.Write("{0} ", sum);
+//         count++;
+//         SummRec(secondNumber, sum, count, element);
+//     }
+// }
 
-int firstNumber = 4;
-int secondNumber = 6;
-int element = 7;
+// int firstNumber = 4;
+// int secondNumber = 6;
+// int element = 7;
 
-Console.Write($"{firstNumber} {secondNumber} ");
-SummRec(firstNumber, secondNumber, 2, element);
+// Console.Write($"{firstNumber} {secondNumber} ");
+// SummRec(firstNumber, secondNumber, 2, element);
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// void ShowNumbers(int number1, int number2, int howmanytimes, bool firstTime)
+// {
+//     if(howmanytimes>1)
+//     {
+//         if(firstTime)
+//         {
+//             Console.Write($"{number1} {number2} ");
+//             firstTime=false;
+//             ShowNumbers(number2,number1+number2,howmanytimes-1,firstTime);
+//         }
+//         else
+//         {
+//             Console.Write($"{number2} ");
+//             ShowNumbers(number2,number1+number2,howmanytimes-1,firstTime);
+//         }
+//     }
+// }
+// Console.WriteLine("Введите число номер 1");
+// int usernumber1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число номер 2");
+// int usernumber2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Сколько чисел показать?");
+// int userEnter = Convert.ToInt32(Console.ReadLine());
+// bool firstTime = true;
+// ShowNumbers(usernumber1,usernumber2,userEnter,firstTime);
