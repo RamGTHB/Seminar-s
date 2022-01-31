900dc703e33337ed042dd5d655b3c630e28711a7
RamGT <n1g9ab@gmail.com>
2022-01-31 22:22:31 +0300

    add sem010

---
+++ Sem010/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Clear();
+
+// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. Первые два элемента последовательности задаются пользователем
+
+void SummRec(int firstNumber, int secondNumber, int count, int element)
+{
+    if (count < element)
+    {
+        int sum = firstNumber + secondNumber;
+        Console.Write("{0} ", sum);
+        count++;
+        SummRec(secondNumber, sum, count, element);
+    }
+}
+
+int firstNumber = 4;
+int secondNumber = 6;
+int element = 7;
+
+Console.Write($"{firstNumber} {secondNumber} ");
+SummRec(firstNumber, secondNumber, 2, element);
